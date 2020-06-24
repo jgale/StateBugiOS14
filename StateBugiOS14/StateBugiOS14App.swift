afa19dfd5ec24b3b7a7df702f8be78695ec49605
Jeremy Gale <jeremy.gale@atco.com>
2020-06-24 10:19:34 -0600

    Initial Commit

---
+++ StateBugiOS14/StateBugiOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  StateBugiOS14App.swift
+//  StateBugiOS14
+//
+//  Created by Jeremy Gale on 2020-06-24.
+//
+
+import SwiftUI
+
+@main
+struct StateBugiOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
